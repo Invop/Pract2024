6b16f61e27098729b22dd3bf4ce0a6680662efa3
pando4kaa <tuhaianastasia@gmail.com>
2024-05-28 22:09:53 +0300

    started working with edit author page

---
+++ ManekiApp/Client/Pages/EditAuthor.razor.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+
+namespace ManekiApp.Client.Pages
+{
+    public partial class EditAuthor
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+        
+        [Inject]
+        protected ManekiAppDBService ManekiAppDBService { get; set; }
+        
+        protected ManekiApp.Server.Models.ManekiAppDB.AuthorPage authorPage;
+        protected string error;
+        protected bool errorVisible;
+        
+        private async Task<ODataServiceResult<Server.Models.ManekiAppDB.AuthorPage>> GetAuthorPagesOData(string userId)
+        {
+            var filter = $"UserId eq '{userId}'";
+            var authorPagesOData = await ManekiAppDBService.GetAuthorPages(filter: filter);
+            return authorPagesOData;
+        }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authorPagesOData = await GetAuthorPagesOData(Security.User.Id);
+            if (authorPagesOData.Value.Any())
+            {
+                authorPage = authorPagesOData.Value.First();
+            }
+            else
+            {
+                NavigationManager.NavigateTo("/create-author-page");
+            }
+            
+        }
+
+        protected async Task FormSubmit(ManekiApp.Server.Models.ManekiAppDB.AuthorPage authorPage)
+        {
+            try
+            {
+                await ManekiAppDBService.UpdateAuthorPage(authorPage.Id, authorPage);
+            }
+            catch (Exception ex)
+            {
+                errorVisible = true;
+                error = ex.Message;
+            }
+        }
+
+        protected async Task CancelClick()
+        {
+            DialogService.Close(null);
+        }
+
+        protected async Task UploadProfileImage(InputFileChangeEventArgs e)
+        {
+            var file = e.File;
+            var buffer = new byte[file.Size];
+            await file.OpenReadStream().ReadAsync(buffer);
+            authorPage.ProfileImage = buffer;
+        }
+    }
+}
