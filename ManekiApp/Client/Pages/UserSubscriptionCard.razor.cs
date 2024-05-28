f8a8ae13c666535bd1756f9b3f1ce7e7ae6cea5c
Invop <shandrykjj512@gmail.com>
2024-05-28 15:01:24 +0300

    unsubscription logic

---
+++ ManekiApp/Client/Pages/UserSubscriptionCard.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+namespace ManekiApp.Client.Pages;
+
+public partial class UserSubscriptionCard
+{
+
+}
--- ManekiApp/Client/Pages/UserSubscriptions.razor.cs
+++ ManekiApp/Client/Pages/UserSubscriptions.razor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ManekiApp.Server.Models.ManekiAppDB;
 using Microsoft.JSInterop;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -32,5 +33,41 @@ namespace ManekiApp.Client.Pages
 
         [Inject]
         protected SecurityService Security { get; set; }
+        [Inject] protected ManekiAppDBService ManekiAppDB { get; set; }
+
+
+        private IEnumerable<UserSubscription> userSubscriptions = new List<UserSubscription>();
+        private IEnumerable<Subscription> subscriptions = new List<Subscription>();
+
+
+        private string GetSubscriptionTitle(Guid userSubscriptionId)
+        {
+            
+            return subscriptions.FirstOrDefault(s => s.Id == userSubscriptionId)?.Title;
+        }
+        private string GetSubscriptionPrice(Guid userSubscriptionId)
+        {
+            
+            return subscriptions.FirstOrDefault(s => s.Id == userSubscriptionId)?.Price + "$";
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var userId = Security.User.Id;
+            var filter = $"UserId eq '{userId}'";
+            var userSubscriptionsOData = await ManekiAppDB.GetUserSubscriptions(filter: filter);
+            userSubscriptions = userSubscriptionsOData.Value;
+            var userSubscriptionIds = userSubscriptions.Select(sub => sub.SubscriptionId).ToList();
+            var idsFilter = string.Join(" or ", userSubscriptionIds.Select(id => $"Id eq {id}"));
+            var subscriptionsOData = await ManekiAppDB.GetSubscriptions(filter: idsFilter);
+
+            subscriptions = subscriptionsOData.Value;
+        }
+        
+        private async Task CancelSubscription(UserSubscription userSubscription)
+        {
+            userSubscription.IsCanceled = true;
+            await ManekiAppDB.UpdateUserSubscription(userSubscription.Id, userSubscription);
+        }
     }
 }
