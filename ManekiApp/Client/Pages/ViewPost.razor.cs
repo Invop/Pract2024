dc6afa8beb58f301c8c657cbf5854165e19d818d
r-rin <rafikov.rinat.m@gmail.com>
2024-05-30 23:35:27 +0300

    Create post view page If post with such id doesn't exist, then it shows page not found

--- ManekiApp/Client/Pages/ViewPost.razor.cs
+++ ManekiApp/Client/Pages/ViewPost.razor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ManekiApp.Server.Models.ManekiAppDB;
 using Microsoft.JSInterop;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
@@ -32,5 +33,52 @@ namespace ManekiApp.Client.Pages
 
         [Inject]
         protected SecurityService Security { get; set; }
+
+        [Inject] 
+        protected ManekiAppDBService ManekiAppDbService { get; set; }
+        
+        [Parameter]
+        public string Id { get; set; }
+
+        protected bool PostExists = true;
+        protected Server.Models.ManekiAppDB.Post Post = new Post();
+        protected Server.Models.ManekiAppDB.AuthorPage Author = new Server.Models.ManekiAppDB.AuthorPage();
+        
+        private async Task<ODataServiceResult<Server.Models.ManekiAppDB.Post>> GetPostById(Guid postId)
+        {
+            var filter = $"Id eq {postId}";
+            var postOData = await ManekiAppDbService.GetPosts(filter: filter);
+            return postOData;
+        }
+
+        private async Task<ODataServiceResult<Server.Models.ManekiAppDB.AuthorPage>> GetAuthorById(Guid authorId)
+        {
+            var filter = $"Id eq {authorId}";
+            var authorOData = await ManekiAppDbService.GetAuthorPages(filter: filter);
+            return authorOData;
+        }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            Guid postId;
+            if (!Guid.TryParse(Id, out postId))
+            {
+                PostExists = false;
+                return;
+            }
+            
+            var postOData = await GetPostById(postId);
+            if (postOData.Value.Any())
+            {
+                Post = postOData.Value.First();
+                var authorOData = await GetAuthorById(Post.AuthorPageId);
+                Author = authorOData.Value.FirstOrDefault();
+            }
+            else
+            {
+                PostExists = false;
+            }
+            
+        }
     }
 }
