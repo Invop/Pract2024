bd263d01924111d0e97f7073db95ba7eb9bf2683
Invop <shandrykjj512@gmail.com>
2024-06-06 14:03:09 +0300

    hangfire jobid

---
+++ ManekiApp/Server/Data/Migrations/20240606110209_JobId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ManekiApp.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class JobId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JobId",
+                schema: "public",
+                table: "UserSubscription",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobId",
+                schema: "public",
+                table: "UserSubscription");
+        }
+    }
+}
