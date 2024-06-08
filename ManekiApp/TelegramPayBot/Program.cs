4ad8e25e3744d80a0b128b8cc40ff4b7810e2282
Invop <shandrykjj512@gmail.com>
2024-06-08 17:47:39 +0300

    run app on 5003 and 5004 ports

--- ManekiApp/TelegramPayBot/Program.cs
+++ ManekiApp/TelegramPayBot/Program.cs
@@ -2,22 +2,25 @@
 
 using Hangfire;
 using Hangfire.PostgreSql;
-using Microsoft.EntityFrameworkCore;
 using ManekiApp.TelegramPayBot;
-using ManekiApp.TelegramPayBot.Keyboard;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://localhost:5003", "https://localhost:5004");
+
 
 builder.Services.AddSingleton<UserSubscriptionJobManager>();
 builder.Services.AddDbContext<ApplicationIdentityDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("ManekiAppDBConnection")));
 
 
-builder.Services.AddEntityFrameworkNpgsql().AddDbContext<HangfireDbContext>(options => {
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<HangfireDbContext>(options =>
+{
     options.UseNpgsql(builder.Configuration.GetConnectionString("HangfireDBConnection"));
 });
 
-builder.Services.AddHangfire(x => 
+builder.Services.AddHangfire(x =>
     x.UseSimpleAssemblyNameTypeSerializer()
         .UseRecommendedSerializerSettings()
         .UsePostgreSqlStorage(builder.Configuration.GetConnectionString("HangfireDBConnection")));
